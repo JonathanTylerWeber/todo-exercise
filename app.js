dbb213bcf1b928b3806657fb70bf5c166bf243b0
JonathanTylerWeber <jonathantweber@gmail.com>
2023-08-06 18:00:05 +0800

    done

--- app.js
+++ app.js
@@ -3,30 +3,14 @@ const input = document.querySelector('#list-item');
 const todoList = document.querySelector('#todo-list');
 let storage = [];
 
-if(localStorage.getItem('todoList')){
-    storage = JSON.parse(localStorage.getItem('todoList'));
-    for (item of storage){
-        const newTodo = document.createElement('li');
-        newTodo.innerText = item;
-        const removeBtn = document.createElement('button');
-        removeBtn.innerText = 'X';
-        newTodo.append(removeBtn);
-        todoList.append(newTodo);
-    }
-};
-
 todoList.addEventListener('click', function(e){
     if(e.target.tagName === 'BUTTON'){
         e.target.parentElement.remove();
-        const todoText = e.target.parentElement.innerText;
-        const index = storage.indexOf(todoText);
-        if (index !== -1){
-            storage.splice(index, 1);
-            updateLocalStorage();
-        }
+        updateLocalStorage();
     }
     else if (e.target.tagName === 'LI'){
         e.target.classList.toggle('done');
+        updateLocalStorage();
     }
 });
 
@@ -35,15 +19,19 @@ form.addEventListener('submit', function(e){
     const newTodo = document.createElement('li');
     newTodo.innerText = input.value;
     input.value = '';
-    storage.push(newTodo.innerText);
-    updateLocalStorage();
     const removeBtn = document.createElement('button');
     removeBtn.innerText = 'X';
     newTodo.append(removeBtn);
     todoList.append(newTodo);
+    updateLocalStorage()
 });
 
 function updateLocalStorage() {
-    localStorage.setItem('todoList', JSON.stringify(storage));
-}
+    localStorage.setItem('todoList', todoList.innerHTML);
+};
 
+function showTask() {
+    todoList.innerHTML = localStorage.getItem('todoList')
+};
+
+showTask()
